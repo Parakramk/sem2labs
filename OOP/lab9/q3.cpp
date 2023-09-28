482f567c25fa6d3e6e78c6e0b1aeeac9030e6e19
Parakram <parakram@keemail.me>
2023-09-28 18:22:50 +0545

    ..

--- OOP/lab9/q3.cpp
+++ OOP/lab9/q3.cpp
@@ -1,24 +1,8 @@
-/*
-3. Develop a simple Banking System with two classes: Account and
-Transaction. The Account class represents bank accounts, including private
-data members for accountNumber, accountHolder, and balance, along with
-public member functions for initialization and data retrieval. The Transaction
-class handles individual transactions, containing private data members
-accountNumber, transactionType, and amount, and a constructor for
-transaction setup. Your task is to create a Friend function named
-processTransaction that links the Account and Transaction classes. This
-function should update the account balance based on the transaction type
-(deposit or withdrawal) and return the updated account balance. Implement
-the classes and function in C++ and demonstrate the banking system's
-functionality with appropriate test cases
-*/
-
 #include<iostream>
 #include<string>
-
 using namespace std;
 
-class Account; // Forward declaration of Account class
+class Account;
 
 class Transaction {
 private:
@@ -32,9 +16,9 @@ public:
         transactionType = tType;
         amount = amt;
     }
-
-    friend void processTransaction(Account& acc, const Transaction& t);
+    friend double processTransaction(Account& acc, const Transaction& t);
 };
+
 class Account {
 private:
     string accountNumber;
@@ -47,60 +31,70 @@ public:
         accountHolder = accHolder;
         balance = bal;
     }
-
-    void displayInfo() const {
-        cout << "Account Number: " << accountNumber << endl;
-        cout << "Account Holder: " << accountHolder << endl;
-        cout << "Balance: " << balance << endl;
+    string getAccountNumber() {
+        return accountNumber;
+    }
+    string getAccountHolder() {
+        return accountHolder;
+    }
+    double getBalance() {
+        return balance;
     }
 
-    void displayBalance() {
-        cout << "Hello " << accountHolder << "! Your updated balance is: " << balance << endl;
-    }
+    // void displayInfo() const {
+    //     cout << "Account Number: " << accountNumber << endl;
+    //     cout << "Account Holder: " << accountHolder << endl;
+    //     cout << "Balance: " << balance << endl;
+    // }
 
-    friend void processTransaction(Account& acc, const Transaction& t);
+    friend double processTransaction(Account& acc, const Transaction& t);
 };
 
-void processTransaction(Account& acc, const Transaction& trans) {
+double processTransaction(Account& acc, const Transaction& trans) {
     if (acc.accountNumber == trans.accountNumber) {
         if (trans.transactionType == 'D' || trans.transactionType == 'd') {
             (trans.amount > 0) ? (acc.balance += trans.amount, cout << "Deposit Succesful" << endl) : cout << "Invalid amount for deposit!" << endl;
         }
         else if (trans.transactionType == 'W' || trans.transactionType == 'w') {
-            (acc.balance >= trans.amount) ? (acc.balance -= trans.amount, cout << "Withdrawal successful." << endl) : cout << "Insufficient balance!" << endl;
+            (acc.balance > trans.amount) ? (acc.balance -= trans.amount, cout << "Withdrawal successful." << endl) : cout << "Insufficient balance!" << endl;
         }
         else {
             cout << "Invalid Transaction Type" << endl;
         }
+        return acc.balance;
     }
-    else {https://pastefy.app/yrgbL3vs
-        cout << "Account Number doesn't Match, Invalid Transaction" << endl;
+    else {
+        // cout << "Account Number doesn't Match, Invalid Transaction" << endl;
+        return 0.0;
     }
-
 }
 
 int main() {
     Account Acc("Acc1", "ABC XYZ", 900);
-    Acc.displayInfo();
+    cout << "Account Holder: " << Acc.getAccountHolder() << endl;
+    cout << "Account Number: " << Acc.getAccountNumber() << endl;
+    cout << "Initial Balance: " << Acc.getBalance() << endl;
 
     Transaction deposit("Acc", 'D', 1000.0);
     Transaction withdrawal("Acc1", 'W', 500.0);
 
-    processTransaction(Acc, deposit);
-    Acc.displayBalance();
+    double updatedBalance = processTransaction(Acc, deposit);
+    (updatedBalance != 0) ? (cout << "Updated Balance after Deposit: " << updatedBalance << endl) : cout << "Account Number doesn't Match, Invalid Transaction" << endl;
 
-    processTransaction(Acc, withdrawal);
-    Acc.displayBalance();
+
+    updatedBalance = processTransaction(Acc, withdrawal);
+    (updatedBalance != 0) ? (cout << "Updated Balance after Withdrawl: " << updatedBalance << endl) : cout << "Account Number doesn't Match, Invalid Transaction" << endl;
 
     return 0;
 }
+
+
 /*
-SAMPLE OUTPUT:
-Account Number: Acc1
+OUTPUT:
 Account Holder: ABC XYZ
-Balance: 900
+Account Number: Acc1
+Initial Balance: 900
 Account Number doesn't Match, Invalid Transaction
-Hello ABC XYZ! Your updated balance is: 900
 Withdrawal successful.
-Hello ABC XYZ! Your updated balance is: 400
+Updated Balance after Withdrawl: 400
 */
